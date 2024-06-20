da2fc0f28bbd1d7465b6468eaa33b9d4d321635f
avnms <avnmsarma@gmail.com>
2024-06-20 10:42:35 +1000

    Created applications index page

--- controllers/applications.js
+++ controllers/applications.js
@@ -3,4 +3,14 @@ const router = express.Router();
 
 const User = require("../models/user");
 
+// Routes
+router.get("/", (req, res) => {
+  try {
+    res.render("applications/index.ejs");
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
+});
+
 module.exports = router;
